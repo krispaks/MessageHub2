57498ea1aaea8fcf64701a9bd80d0550dc59f503
Khris Allan Paca <krispaks@yahoo.com>
2015-04-30 11:08:16 -0500

    add IRavenMessageUoW

--- MessageHub.Lib/UnitOfWork/IRavenMessageUoW.cs
+++ MessageHub.Lib/UnitOfWork/IRavenMessageUoW.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MessageHub.Lib.Repository;
 using MessageHub.Lib.Entity;
+using MessageHub.Lib.Repository;
 using Raven.Client;
 
 namespace MessageHub.Lib.UnitOfWork
 {
-	public interface IMessageUoW : IUnitOfWork
+	public interface IRavenMessageUoW : IUnitOfWork
 	{
-		IRepository<Message, MessageHubDbContext> MessageHubRepositoryRepository { get; set; }
+		IRepository<Message, IDocumentSession> MessageRavenRepositoryRepository { get; set; }
 	}
 }
